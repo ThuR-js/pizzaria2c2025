24e5b46c54412bee740cd012c7ef29130f3df9e6
rm94448@estudante.fieb.edu.br <rm94448@estudante.fieb.edu.br>
2025-03-19 08:27:51 -0300

    Componente

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,30 @@ function App(){
 
    const [nome, setNome]  = useState('Ronaldo')
 
-  return (
-    <div>
-      <h3>Pizzaria 2C</h3>
-      <input
+  const Formulario = () => {
+     return (
+      <div>
+       <input
       className="nome"
-      onChange={(e)=>{setNome(e.target.value)}}
+      onChange={(e)=>{setNome(e.target.value);}}
       name="nome"
       type="text" />
       <button
-      onClick={()=>{alert(nome)}}
+      onClick={()=>{alert(nome);}}
       className="botao">
      CLIQUE AQUI
      </button>   
+      </div>
+     )
+  }
+
+
+  return (
+    <div>
+      <h3>Pizzaria 2C</h3>
+     <Formulario/> 
+     <Formulario/> 
+     <Formulario/> 
     </div>
   
   )
